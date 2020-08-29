ee68748f2de787ca10705b0a14b8d1465f384d2e
Radoslav Hubenov <rrhubenov@gmail.com>
2020-08-29 22:06:17 +0300

    Alpha version of the program

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@ unsigned wordSetCount;
 char** wordSet; 
 
 unsigned matrixSize;
+char** letterMatrix;
 
-void freeWordSet(char** wordSet, const unsigned wordSetCount) {
+void freeWordSet() {
     for(unsigned i = 0; i < wordSetCount; ++i) {
         delete wordSet[i];
     }
 
     delete[] wordSet;
+}
+
+void freeLetterMatrix() {
+    for(unsigned i = 0; i < matrixSize; ++i) {
+        delete letterMatrix[i];
+    }
+    
+    delete[] letterMatrix;
 }
 
 void allocAndInsert(const char* str, const unsigned pos) {
@@ -31,22 +40,30 @@ void insertionSortByLength() {
     }
 }
 
-bool isInField(unsigned row, unsigned column) {
+bool isInField(int row, int column) {
     if(row < 0 || row >= matrixSize) return false;
     if(column < 0 || column >= matrixSize) return false;
     return true;
 }
 
-unsigned[] findLetterAroundElement(unsigned row, unsigned column, char letter, char** letterMatrix) {
-    const int y[] = { -1, -1, -1,  1, 1, 1,  0, 0 };
-    const int x[] = { -1,  0,  1, -1, 0, 1, -1, 1 };
+unsigned* findLetterAroundElement(unsigned row, unsigned column, char letter, bool visitedNeighbours[8]) {
+    const int y[] = { -1, -1, -1, 0, 1, 1,  1, 0 };
+    const int x[] = { -1,  0,  1, 1, 1, 0, -1, -1 };
 
     for(int i = 0; i < 8; ++i) {
-        unsigned currentRow = row + y[i];
-        unsigned currentColumn = column + x[i];
+        int currentRow = row + y[i];
+        int currentColumn = column + x[i];
+
+        //Insure pos is in matrix
         if(isInField(currentRow, currentColumn)) {
-            if(letter == letterMatrix[currentRow][currentColumn]) {
-                unsigned point = { row, column };
+            if(letter == letterMatrix[currentRow][currentColumn] && visitedNeighbours[i] == false) {
+                visitedNeighbours[i] = true;
+
+                unsigned* point = new unsigned[2];
+
+                point[0] = currentRow;
+                point[1] = currentColumn;
+
                 return point;
             }
         }
@@ -55,62 +72,57 @@ unsigned[] findLetterAroundElement(unsigned row, unsigned column, char letter, c
     return NULL;
 }
 
-bool isWordInMatrix(char* word, char letterMatrix[][matrixSize]) {
+bool isWordInMatrix(unsigned row, unsigned column, char* word) {
 
-    char firstLetter = word[0];
+    if(strlen(word) == 0){
+        return true;
+    } 
 
-    for(unsigned i = 0; i < matrixSize*matrixSize; ++i) {
-        if(firstLetter == letterMatrix[i]) {
-            unsigned row = (i / matrixSize) + 1;
-            unsigned column = (i % matrixSize) + 1;
+    bool visitedNeighbours[8] = {false};
 
-            //Make a copy of the matrix
-            char matrixCopy[matrixSize][matrixSize];
-            for(unsigned x = 0; x < matrixSize; ++x) {
-                for(unsigned y = 0; y < matrixSize; ++y) {
-                    matrixCopy[x][y] = letterMatrix[x][y];
-                }
-            }
+    while(true) {
+        unsigned* point = findLetterAroundElement(row, column, word[0], visitedNeighbours);
 
-            for(unsigned j = 1; j < strlen(word); ++j) {
-                unsigned rowAndColumn[2] = findLetterAroundElement(row, column, word[j], letterMatrix);
+        if(point == NULL) return false;
+        else {
+            unsigned pointRow = point[0];
+            unsigned pointColumn = point[1];
 
-                if(next == NULL) {
-                    break;
-                }
-                else if(j == strlen(word)) {
-                    return true;
-                }
-                else {
-                    row = rowAndColumn[0];
-                    column = rowAndColumn[1];
-                    letterMatrix[row][column] = '0';
-                }
-            }
+            delete[] point;
+
+            char* restOfWord = word + 1;
+
+            if(isWordInMatrix(pointRow, pointColumn, restOfWord)) 
+                return true;
         }
     }
-
-    return false;
 }
 
 int main() {
     std::cout << "Enter word set size: ";
     std::cin >> wordSetCount;
 
-    wordSet = new char*[wordSetCount];
+    wordSet = new (std::nothrow) char*[wordSetCount];
 
+    if(wordSet == NULL) {
+        std::cout << "Memory issue...\n";
+        return -1;
+    }
+
+    // Input logic
     for(unsigned i = 0; i < wordSetCount; ++i) {
         std::cout << "Enter word " << i << ": ";
         char temp[100];
         std::cin >> temp;
 
+        // Insure input is lex sorted
         if(i == 0) {
             allocAndInsert(temp, 0);
         } else if(strcmp(temp, wordSet[i-1]) > 0) {
             allocAndInsert(temp, i);
         } else {
             std::cout << "Not lex sorted...";
-            return -1;
+            return 0;
         }
     }
 
@@ -120,13 +132,27 @@ int main() {
 
     std::cin >> matrixSize;
 
-    char letterMatrix[matrixSize][matrixSize];
+    letterMatrix = new (std::nothrow) char*[matrixSize];
+
+    if(letterMatrix == NULL) {
+        std::cout << "Memory issue...\n";
+        return -1;
+    }
 
     for(unsigned column = 0; column < matrixSize; ++column) {
+        char* columnString = new (std::nothrow) char[matrixSize];
+
+        if(columnString == NULL) {
+            std::cout << "Memory issue...\n";
+            return -1;
+        }
+
+        letterMatrix[column] = columnString;
+
         for(unsigned row = 0; row < matrixSize; ++row) {
             char temp;
             std::cin >> temp;
-            letterMatrix[column][row] = temp;
+            columnString[row] = temp;
         }
     }
 
@@ -136,17 +162,33 @@ int main() {
     for(unsigned i = 0; i < wordSetCount; ++i) {
         char* wordSearched = wordSet[i];
 
-        if(strlen(wordSearched) > matrixSize*matrixSize)
+        if(strlen(wordSearched) > matrixSize*matrixSize) {
+            std::cout << "Word " << wordSearched << " is longer than matrix, skipping...\n";
             continue;
+        }
 
-        if(isWordInMatrix(wordSearched, letterMatrix)) {
-            wordFoundLength = strlen(wordSearched);
+        std::cout << "Checking for word: " << wordSearched << "\n";
+        
+        for(unsigned i = 0; i < matrixSize*matrixSize; ++i) {
+            unsigned row = (i / matrixSize);
+            unsigned column = (i % matrixSize);
+            if(wordSearched[0] == letterMatrix[row][column]) {
+
+                std::cout << "Letter '" << wordSearched[0] << "' found on pos: (" << row << "," << column << ")\n" ;
+
+                char* restOfWord = wordSearched + 1;
+
+                if(isWordInMatrix(row, column, restOfWord)) {
+                    std::cout << "Word: " << wordSearched << " has been found!\n";
+                    wordFoundLength = strlen(wordSearched);
+                }
+            }
         }
     }
 
     std::cout << wordFoundLength << "\n";
 
-
-    freeWordSet(wordSet, wordSetCount);
+    freeWordSet();
+    freeLetterMatrix();
     return 0;
 }
